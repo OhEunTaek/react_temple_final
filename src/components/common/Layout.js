573557874ad4d09daeef529afbc4d3de217a90e6
OhEunTaek <giloet777@gmail.com>
2023-02-05 21:51:23 +0900

    axios설치 및 컴포넌트가 처음 마운트 되었을때 한번만 데이터를 요청하여 받은 데이터를 useState를 이용 state로 관리, state에 담긴 값으로 동적 가상돔 생성

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,12 +1,8 @@
 import { useEffect, useRef } from "react";
 function Layout(props) {
-    //props는 부모로부터 전달받은 인수값
+
     const frame = useRef(null);
-    //useRef사용하는 방법
-    //1. 리액트로부터 useRef를 임포트한다
-    //2. 변수에 useRef의 초기값을 설정해 담는다
-    //3. 우리가 참조하고자 하는 곳에 ref={변수}을 쓴다
-    // useRef는 current라는 공간안에 해당 값을 저장한다
+
     console.log(frame);
 
     useEffect(() => {
