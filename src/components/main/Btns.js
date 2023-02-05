0c44c6e69f482ac3d179d10a4b6d98bd0549478b
OhEunTaek <giloet777@gmail.com>
2023-02-05 23:19:39 +0900

    Scroll : clean-up함수로 언마운트시 스크롤 이벤트 제거 + 스크롤시 버튼, 섹션 활성화

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -4,21 +4,47 @@ function Btns() {
     const pos = useRef([]);
     const btnRef = useRef(null);
 
+    //세로 위치값 갱신 함수
     const getPos = () => {
         pos.current = [];
         const secs = btnRef.current.parentElement.querySelectorAll('.myScroll');
         for (const sec of secs) pos.current.push(sec.offsetTop);
-        console.log(pos.current);
+    };
+
+    //버튼, 박스 활성화 함수
+    const activation = () => {
+        const btns = btnRef.current.children;
+        const secs = btnRef.current.parentElement.querySelectorAll('.myScroll');
+        const scroll = window.scrollY;
+        const base = -window.innerHeight / 3;
+
+        pos.current.forEach((pos, idx) => {
+            if (scroll >= pos + base) {
+                for (const btn of btns) btn.classList.remove('on');
+                for (const sec of secs) sec.classList.remove('on');
+                btns[idx].classList.add('on');
+                secs[idx].classList.add('on');
+            }
+        });
     };
 
     useEffect(() => {
         getPos();
         window.addEventListener('resize', getPos);
+        window.addEventListener('scroll', activation);
+
+        return () => {
+            //window객체는 최상의 전역 객체이기 때문에..
+            //윈도우객체에 이벤트연결시 해당 이벤트가 다른 컴포넌트 페이지에도 실행되는게 아니라면
+            //해당 컴포넌트가 unmount될떄 clean-up함수로 이벤트 연결을 제거
+            window.removeEventListener('resize', getPos);
+            window.removeEventListener('scroll', activation);
+        };
     }, []);
 
     return (
         <ul className='scroll_navi' ref={btnRef}>
-            <li></li>
+            <li className='on'></li>
             <li></li>
             <li></li>
             <li></li>
