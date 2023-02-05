8eae8529d7a71e977a996fa462f5ef2bf07e0230
OhEunTaek <giloet777@gmail.com>
2023-02-05 21:59:15 +0900

    유튜브 데이터 호출

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,5 +1,18 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function Youtube() {
+	useEffect(() => {
+		const key = 'AIzaSyDq1ThuKd63CGMc178rIvnscNriIww6L4A';
+		const playlist = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+		const num = 10;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
+
+		axios.get(url).then((json) => {
+			console.log(json);
+		});
+	}, []);
 	return (
 		<Layout name={'Youtube'}>
 			<p>Youtube Content</p>
