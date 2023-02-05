aa62fe459955d02fec00fd9b433b85d032cf9e96
OhEunTaek <giloet777@gmail.com>
2023-02-06 00:20:47 +0900

    location: 지도의 정보가 담긴 배열의 갯수만큼 버튼을 동적생성하고 버튼 클릭시 순번으로 변경되도록 index state생성

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -31,43 +31,35 @@ function Location() {
 
 
 	const [Traffic, setTraffic] = useState(false);
-	//지도 인스턴스값을 담을 참조객체 생성
+
+	//지점 버튼 클릭시 변경할 순번이 담길 state
+	const [Index, setIndex] = useState(0);
+
 	const mapInstance = useRef(null);
-	const [Location, setLocation] = useState(null);
 	const container = useRef(null);
-	// const { kakao } = window; 위로 올림
 	const option = {
-		center: new kakao.maps.LatLng(37.40211707077346, 127.10344953763003),
+		center: info[Index].latlng,
 		level: 3,
 	};
 
-
-	// const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
-	// const imageSize = new kakao.maps.Size(232, 99);
-	// const imageOption = { offset: new kakao.maps.Point(116, 99) };
-	const imageSrc = info[0].imgUrl;
-	const imageSize = info[0].imgSize;
-	const imageOption = info[0].imgPos;
-
+	const imageSrc = info[Index].imgUrl;
+	const imageSize = info[Index].imgSize;
+	const imageOption = info[Index].imgPos;
 
 	const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 
 
 	const marker = new kakao.maps.Marker({
-		position: info[0].latlng,
+		position: info[Index].latlng,
 		image: markerImage,
 	});
 
 	useEffect(() => {
-		//지도인스턴스를 참조객체에 옮겨담음
 		mapInstance.current = new kakao.maps.Map(container.current, option);
-		//참조객체에 담긴 인스턴스 값으로 마커 세팅
 		marker.setMap(mapInstance.current);
 
 	}, []);
 	useEffect(() => {
-		//Traffic state가 변경될때마다 참조객체에 담긴 instance로 부터 교통정보량 출력, 제거
-		//인스턴스를 state에 옮겨담는것이 아닌 참고객체에 바로 전달하므로 Optional Chaining 불필요
 		Traffic
 			? mapInstance.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
 			: mapInstance.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
@@ -75,6 +67,21 @@ function Location() {
 	return (
 		<Layout name={'Location'}>
 			<div id='map' ref={container}></div>
+			<button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic ON' : 'Traffic OFF'}</button>
+			{/* 배열의 정보값으로 동적으로 버튼을 생성하고 */}
+			{info.map((el, idx) => {
+				return (
+					<li
+						key={idx}
+						onClick={() => {
+							//각 버튼 클릭시 클릭한 순번으로 Index state변경
+							setIndex(idx);
+						}}
+					>
+						{el.title}
+					</li>
+				);
+			})}
 		</Layout>
 	);
 }
