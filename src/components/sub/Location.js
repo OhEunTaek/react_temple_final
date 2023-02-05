b6c1f47c942b7dcf19e4e21583177852afcdc6f7
OhEunTaek <giloet777@gmail.com>
2023-02-06 00:08:17 +0900

    location: traffic값이 변경될 때마다 지도 인스턴스 생성

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,7 +1,11 @@
 import Layout from '../common/Layout';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 function Location() {
+	//토글 버튼을 클릭할때마 변경될 state
+	const [Traffic, setTraffic] = useState(false);
+	//지도 인스턴스를 담을 state
+	const [Location, setLocation] = useState(null);
 	//지도가 담길 프레임 참조객체
 	const container = useRef(null);
 	//윈도우 객체로부터 kakao객체 직접 비구조화할당으로 추출
@@ -31,7 +35,16 @@ function Location() {
 		const map_instance = new kakao.maps.Map(container.current, option);
 		//마커인스턴스로부터 setMap메서드를 호출하고 인스턴스로 지도 기본 인스턴스값 전달
 		marker.setMap(map_instance);
+		setLocation(map_instance);
 	}, []);
+	useEffect(() => {
+		//Traffic가 변경될때마다
+		//해당 true 면 교통정보출력, 그렇지않으면 교통정보 제거
+		//옵셔널 체이닝: ?앞의 객체정보값이 있을때만 다음 메서드구문을 실행하고 그렇지 않으면 해당 코드 무시
+		Traffic
+			? Location?.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+			: Location?.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+	}, [Traffic]);
 	return (
 		<Layout name={'Location'}>
 			<div id='map' ref={container}></div>
