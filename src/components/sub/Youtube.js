3129d5ea7f2c6bc6fb7a19477c72241f04a10582
OhEunTaek <giloet777@gmail.com>
2023-02-05 22:00:17 +0900

    유튜브 리스트 출력

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 function Youtube() {
+	const [Vids, setVids] = useState([]);
 	useEffect(() => {
 		const key = 'AIzaSyDq1ThuKd63CGMc178rIvnscNriIww6L4A';
 		const playlist = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
@@ -10,12 +11,28 @@ function Youtube() {
 		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
 
 		axios.get(url).then((json) => {
-			console.log(json);
+			console.log(json.data.items);
+			setVids(json.data.items);
 		});
 	}, []);
 	return (
 		<Layout name={'Youtube'}>
-			<p>Youtube Content</p>
+			{Vids.map((data) => {
+				return (
+					<article key={data.id}>
+						<h3>{data.snippet.title}</h3>
+
+						<div className='txt'>
+							<p>{data.snippet.description}</p>
+							<span>{data.snippet.publishedAt}</span>
+						</div>
+
+						<div className='pic'>
+							<img src={data.snippet.thumbnails.high.url} alt={data.snippet.title} />
+						</div>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
