daea1ded1f941e9917b92e3ad7738b99d2763f17
OhEunTaek <giloet777@gmail.com>
2023-02-05 20:01:32 +0900

    템블릿폴더 만드는 순서 주석 및 ssr vs csr 주석

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
     </div>
   );
 }
 
 export default App;
+
+/*
+SSR vs CSR
+
+SSR = Server side Rendering
+-페이지 이동시마다 일일 서버쪽에 출력될  html 파일을 요청하는 방법
+-장점 : 초기 로딩속도가 빠른편, 검색최적화된 방법
+-단점 : 페이지 이동시, 같은 페이지 안에서 컨텐츠가 동적으로 바뀌어야 될때 사용성이 낮아서 불편
+
+CSR = Client side Rendering
+- 초기에 화면에 출력될 모든 정보 데이터를 chunk단위로 모두 불러옴
+-장점 : 같은 페이지 안에서 서로 다른 컨텐츠를 실시간으로 변경하면서 출력되므로 속도가 빠름(다른컨텐츠를 실시간으로 보여주는 속도)
+-단점 : 초기 로딩속도가 느림, 검색엔진 비최적화
+
+*/
