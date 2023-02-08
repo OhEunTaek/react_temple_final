2221446efe9f234d5a1618f23e290b8f2394e7ed
OhEunTaek <giloet777@gmail.com>
2023-02-08 23:42:18 +0900

    main visual 영역 콜백이용 박스모션

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,5 +1,81 @@
+import { useRef } from 'react';
+import Anime from '../../asset/plugIn';
+
 function Visual() {
-	return <figure id='visual' className='myScroll'></figure>;
+	const rect = useRef(null);
+	const speed = 1000;
+
+	const openBox = () => {
+		const [top, right, bottom, left, con] = rect.current.children;
+		new Anime(top, {
+			prop: 'width',
+			value: '100%',
+			duration: speed,
+			callback: () => {
+				new Anime(right, {
+					prop: 'height',
+					value: '100%',
+					duration: speed,
+					callback: () => {
+						new Anime(bottom, {
+							prop: 'width',
+							value: '100%',
+							duration: speed,
+							callback: () => {
+								new Anime(left, {
+									prop: 'height',
+									value: '100%',
+									duration: speed,
+									callback: () => {
+										new Anime(con, {
+											prop: 'opacity',
+											value: 1,
+											duration: speed * 2,
+										});
+									},
+								});
+							},
+						});
+					},
+				});
+			},
+		});
+	};
+
+	const closeBox = () => {
+		const [top, right, bottom, left, con] = rect.current.children;
+
+		new Anime(con, {
+			prop: 'opacity',
+			value: 0,
+			duration: speed,
+			callback: () => {
+				new Anime(top, { prop: 'width', value: '0%', duration: speed });
+				new Anime(left, { prop: 'height', value: '0%', duration: speed });
+				new Anime(right, { prop: 'height', value: '0%', duration: speed });
+				new Anime(bottom, { prop: 'width', value: '0%', duration: speed });
+			},
+		});
+	};
+
+	return (
+		<figure id='visual' className='myScroll'>
+			<button className='btnOpen' onClick={openBox}>
+				open
+			</button>
+			<div className='react' ref={rect}>
+				<div className='top'></div>
+				<div className='right'></div>
+				<div className='bottom'></div>
+				<div className='left'></div>
+				<div className='con'>
+					<span className='btnClose' onClick={closeBox}>
+						close
+					</span>
+				</div>
+			</div>
+		</figure>
+	);
 }
 
 export default Visual;
