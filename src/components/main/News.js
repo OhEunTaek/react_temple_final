91db6bfefcdeed471e6080835cd2671ea0044586
OhEunTaek <giloet777@gmail.com>
2023-02-08 23:36:22 +0900

    커뮤니티 :main 컴포넌트에 미리보기 데이터 출력-> community의 더미데이터는 생략가능

--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,10 +1,14 @@
+import { useRef } from 'react';
+
 function News({ Scrolled, currentPos }) {
-    // const scroll = Scrolled - currentPos;
+    const getLocalData = () => {
+        const data = localStorage.getItem('post');
+        return JSON.parse(data);
+    };
+    const data = useRef(getLocalData());
     const base = -window.innerHeight / 3;
     let scroll = Scrolled - base - currentPos || 0;
-    //scroll값이 만약 음수값이면 0으로 값으로 고정
-    //scroll값이 음수인 경우는 아직 스크롤 거리값이 해당 컴포넌트 영역에 도달하지 않았을때
-    //아래 구문을 주석으로 묶으면 스크롤이 아직 해당 영역에 도달하지 않아도 scroll의 변화값을 연동
+
     scroll < 0 && (scroll = 0);
 
     return (
@@ -18,6 +22,18 @@ function News({ Scrolled, currentPos }) {
             >
                 Information
             </h2>
+            <div className='wrap'>
+                {data.current.map((data, idx) => {
+                    if (idx >= 3) return null;
+
+                    return (
+                        <article key={idx}>
+                            <h5>{data.title}</h5>
+                            <p>{data.content}</p>
+                        </article>
+                    );
+                })}
+            </div>
         </section>
     );
 }
