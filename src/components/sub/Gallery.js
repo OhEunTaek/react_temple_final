51c994bfc6d933506abc55afc3752cf7e692b4a5
OhEunTaek <giloet777@gmail.com>
2023-02-08 23:51:05 +0900

    gallery : 타입에따라서 url변경함수 생성, 의존배열제거 인기있는 사진 메서드

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -3,20 +3,35 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Gallery() {
-	const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
-	const key = 'ae5dbef0587895ed38171fcda4afb648';
-	const method_interest = 'flickr.interestingness.getList';
-	const num = 20;
-	const url = `${baseURL}&method=${method_interest}&api_key=${key}&per_page=${num}`;
+	// const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
+	// const key = 'ae5dbef0587895ed38171fcda4afb648';
+	// const method_interest = 'flickr.interestingness.getList';
+	// const num = 20;
+	// const url = `${baseURL}&method=${method_interest}&api_key=${key}&per_page=${num}`;
 
+	const getFlickr = async (opt) => {
+		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
+		const key = 'ae5dbef0587895ed38171fcda4afb648';
+		const method_interest = 'flickr.interestingness.getList';
+		const num = 20;
+		let url = '';
+		//만약 인수로 전달받은 opt객체의 type이 'interest'이면
+		//interest방식의 url을 만들어서 axios호출
+		if (opt.type === 'interest') url = `${baseURL}&method=${method_interest}&api_key=${key}&per_page=${num}`;
+
+		const result = await axios.get(url);
+		setItems(result.data.photos.photo);
+	};
 	const [Items, setItems] = useState([]);
 
 	useEffect(() => {
-		axios.get(url).then((json) => {
-			console.log(json.data.photos.photo);
-			setItems(json.data.photos.photo);
-		});
-	}, [url]); //dependency array추가
+		getFlickr({ type: 'interest' });
+		//getFlickr({type: 'search', tags: '하늘'})
+		// axios.get(url).then((json) => {
+		// 	console.log(json.data.photos.photo);
+		// 	setItems(json.data.photos.photo);
+		// });
+	}, []); //dependency array 제거
 
 	return (
 		<Layout name={'Gallery'}>
