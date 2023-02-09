8c38f4437e448cd638a9412ee8c8749c8a28f230
OhEunTeak <giloet777@gmail.com>
2023-02-09 23:39:03 +0900

    modal.js 수정

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,28 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, forwardRef, useState, useImperativeHandle } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-function Modal(props) {
-    useEffect(() => {
-        //모달 컴포넌트 마운트시 스크롤기능 비활성화
-        document.body.style.overflow = 'hidden';
 
-        return () => {
-            //모달 컴포넌트 언마운트시 스크롤기능 다시 활성화 (clean-up)
-            document.body.style.overflow = 'auto';
-        };
-    }, []);
+const Modal = forwardRef((props, ref) => {
+    const [Open, setOpen] = useState(false);
+
+    useImperativeHandle(ref, () => {
+        return { open: () => setOpen(true) };
+    });
+
+    useEffect(() => {
+        Open ? (document.body.style.overflow = 'hidden') : (document.body.style.overflow = 'auto');
+    }, [Open]);
+
     return (
-        // <aside className='modal'>
-        //     <div className='con'>{props.children}</div>
-        //     <span
-        //         className='close'
-        //         onClick={() => {
-        //             //부모로부터 props로 state변경함수를 전달받아서 호출
-        //             props.setOpen(false);
-        //         }}
-        //     >
-        //         close
-        //     </span>
-        // </aside>
         <AnimatePresence>
             {Open && (
                 <motion.aside
@@ -52,6 +42,6 @@ function Modal(props) {
             )}
         </AnimatePresence>
     );
-}
+});
 
 export default Modal;
